4e24274beb94f93fe785ddf531e3a9556508bcf7
ipselium <ipselium@free.fr>
2022-01-07 18:36:39 +0100

    Bump version: 0.8.8 → 0.9.0

--- setup.py
+++ setup.py
@@ -36,7 +36,7 @@ setup(
     description="Grid generator",
     long_description=open('README.rst').read(),
     long_description_content_type='text/x-rst',
-    version='0.8.8',
+    version='0.9.0',
     license="GPL",
     url='http://github.com/ipselium/fdgrid',
     author="Cyril Desjouy",
